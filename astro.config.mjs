ae73ff75fbbf236f539c42ca50534c5498b7d7a1
Thomas More <sir-thomas-more@gmail.com>
2024-01-14 09:58:36 +0100

    Install spotlighr

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,18 @@
 import { defineConfig } from 'astro/config';
 import mdx from "@astrojs/mdx";
 import vercel from '@astrojs/vercel/serverless';
-
 import react from "@astrojs/react";
+
+import sentry from "@sentry/astro";
+import spotlightjs from "@spotlightjs/astro";
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
-  integrations: [mdx(), react()]
+  integrations: [mdx(), react(), sentry(), spotlightjs()]
 });
