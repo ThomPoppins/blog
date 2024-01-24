a723b40ab2fe878fd37ed339b476dd1ca3e206ca
Thomas More <sir-thomas-more@gmail.com>
2024-01-24 02:45:35 +0100

    Fix

--- payload.config.ts
+++
@@ -1,7 +0,0 @@
-import { buildConfig } from 'payload/config'
-
-export default buildConfig({
-  // By default, Payload will boot up normally
-  // and you will be provided with a base `User` collection.
-  // But, here is where you define how you'd like Payload to work!
-})
---
+++ src/collections/Posts.ts
@@ -0,0 +1,33 @@
+import { CollectionConfig } from "payload/types";
+
+
+const Posts: CollectionConfig = {
+  slug: "posts",
+  admin: {
+    useAsTitle: "title",
+  },
+  access: {
+    read: () => true,
+  },
+
+  fields: [
+    {
+      name: "title",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "content",
+      type: "text",
+      required: true,
+    },
+    {
+      name: "slug",
+      type: "text",
+      required: true,
+    },
+  ],
+};
+
+export default Posts;
+
