dbbae7c59ae923b8b3cfd4707c6ee72cf8359859
Thomas More <sir-thomas-more@gmail.com>
2024-01-26 04:41:51 +0100

    Installed Svelte, could be handy sometimes

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,29 +5,27 @@ import react from "@astrojs/react";
 import sentry from "@sentry/astro";
 import spotlightjs from "@spotlightjs/astro";
 
+import svelte from "@astrojs/svelte";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://un-sheeple.me",
   output: "server",
   adapter: vercel({
+    imageService: true,
     webAnalytics: {
-      enabled: true,
-    },
+      enabled: true
+    }
   }),
-  integrations: [
-    mdx(),
-    react(),
-    sentry({
-      dsn: "https://63fa277e17f202ed07d687a3747ce513@o4506631183597568.ingest.sentry.io/4506631292715008",
-      sourceMapsUploadOptions: {
-        telemetry: false,
-        project: "un-sheeple-me",
-        authToken: process.env.SENTRY_AUTH_TOKEN,
-      },
-      options: {
-        telemetry: false,
-      },
-    }),
-    spotlightjs(),
-  ],
+  integrations: [mdx(), react(), sentry({
+    dsn: "https://63fa277e17f202ed07d687a3747ce513@o4506631183597568.ingest.sentry.io/4506631292715008",
+    sourceMapsUploadOptions: {
+      telemetry: false,
+      project: "un-sheeple-me",
+      authToken: process.env.SENTRY_AUTH_TOKEN
+    },
+    options: {
+      telemetry: false
+    }
+  }), spotlightjs(), svelte()]
 });
